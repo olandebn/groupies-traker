cb2203e3ca3e609bcb3ddb4e65b74a1df611a291
olandebn <olan.debruyne@ynov.com>
2026-01-12 12:38:58 +0100

    sqd

--- main.go
+++ main.go
@@ -5,48 +5,57 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strings" // Nécessaire pour manipuler les chaînes de caractères
 )
 
-// Chargement des templates globalement pour éviter de les re-parser à chaque requête
-var tmpls = template.Must(template.ParseGlob("templates/*.html"))
+// On définit ici des fonctions personnalisées utilisables dans le HTML
+var funcMap = template.FuncMap{
+	"cleanLocation": func(s string) string {
+		// Remplace les tirets bas "_" par des espaces
+		s = strings.ReplaceAll(s, "_", " ")
+		// Remplace les tirets "-" par ", "
+		s = strings.ReplaceAll(s, "-", ", ")
+		// Met la première lettre de chaque mot en majuscule
+		s = strings.Title(s)
+		return s
+	},
+}
+
+// Chargement des templates AVEC la FuncMap (très important de le faire dans cet ordre)
+var tmpls = template.Must(template.New("").Funcs(funcMap).ParseGlob("templates/*.html"))
 
 func main() {
-	// Configuration du serveur de fichiers statiques (pour le CSS premium) [cite: 9]
+	// Configuration du serveur de fichiers statiques (pour le CSS)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// Route principale : Affichage de tous les artistes [cite: 3, 5]
+	// Routes
 	http.HandleFunc("/", homeHandler)
-
-	// Route Détails : Action client-serveur pour les relations [cite: 8, 11, 12]
 	http.HandleFunc("/artist", artistHandler)
 
 	fmt.Println("Siuuu! Serveur lancé sur : http://localhost:8080")
-	// Gestion des erreurs fatales pour que le serveur ne crash pas [cite: 18]
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // Handler pour la page d'accueil
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	// Gestion de l'erreur 404 si l'URL est incorrecte [cite: 19]
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
 	var artists []Artist
-	// Appel à l'API via data.go [cite: 4, 14]
+	// Appel à FetchData (défini dans data.go)
 	err := FetchData("/artists", &artists)
 	if err != nil {
 		http.Error(w, "Erreur lors de la récupération des artistes", http.StatusInternalServerError)
 		return
 	}
 
-	// Visualisation des données via le template index.html [cite: 9, 31]
 	tmpls.ExecuteTemplate(w, "index.html", artists)
 }
 
-// Handler pour les détails (Événement client-serveur)
+// Handler pour les détails
 func artistHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	if id == "" {
@@ -54,11 +63,11 @@ func artistHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 1. Récupérer les infos de l'artiste [cite: 5]
+	// 1. Récupérer les infos de l'artiste
 	var artist Artist
 	errArt := FetchData("/artists/"+id, &artist)
 
-	// 2. Récupérer les relations (Dates et Lieux) [cite: 8, 14]
+	// 2. Récupérer les relations (Dates et Lieux)
 	var rel Relation
 	errRel := FetchData("/relation/"+id, &rel)
 
@@ -67,7 +76,7 @@ func artistHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// On combine les données pour le template [cite: 27, 28]
+	// On combine les données pour le template
 	data := struct {
 		Artist   Artist
 		Relation Relation
